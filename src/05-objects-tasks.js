b63870bfed6498b07f8f329932641de1a4c87c8b
zhirkovpetr <zhirkovpetr95@gmail.com>
2022-01-14 14:00:01 +0300

    refactoring: change in objects tasks getArea to arrow function

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -23,9 +23,7 @@
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
-    return this.width * this.height;
-  };
+  this.getArea = () => this.width * this.height;
 }
 
 /**
